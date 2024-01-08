ca8c6d82bbb612af76bd0a115aed238b2e90f50e
netvminc <147425814+netvminc@users.noreply.github.com>
2024-01-08 17:59:47 +0200

    Update common.js

--- src/common.js
+++ src/common.js
@@ -1,16 +1,6 @@
 export const config = {
     serverAddresses: [
-        "wss://computernewb.com/collab-vm/vm0",
-        "wss://computernewb.com/collab-vm/vm1",
-        "wss://computernewb.com/collab-vm/vm2",
-        "wss://computernewb.com/collab-vm/vm3",
-        "wss://computernewb.com/collab-vm/vm4",
-        "wss://computernewb.com/collab-vm/vm5",
-        "wss://computernewb.com/collab-vm/vm6",
-        "wss://computernewb.com/collab-vm/vm7",
-        "wss://computernewb.com/collab-vm/vm8",
-	"wss://computernewb.com/collab-vm/vm9",
-	"wss://computernewb.com/collab-vm/eventvm",
+        "wss://rendervmserver.l5.ca",
     ],
     chatSound: "https://computernewb.com/collab-vm/notify.ogg",
     // What XSS implementation the server uses
